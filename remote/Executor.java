d9c3be2d67abc21fd116441cbbcc8923cbf69a51
EduardAliiev <edeku17@gmail.com>
2016-07-12 21:58:20 +0300

    'PrimitiveConvertor' for converting: floatToChar, intToChar and charToInt

---
+++ remote/Executor.java
@@ -0,0 +1,25 @@
+package com.EduardAliiev.utils.convertors;
+public class Executor
+{
+    public static void main (String args[])
+    {
+        PrimitiveConvertor primitiveConvertor = new PrimitiveConvertor();
+        primitiveConvertor.convertor();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
