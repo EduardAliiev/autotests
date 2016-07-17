f5e78c7a0d2e4ce6c660d21bf01c463c95f70f9d
EduardAliiev <edeku17@gmail.com>
2016-07-17 21:45:32 +0300

    Added condition that entering equal radius

--- src/com/EduardAliiev/utils/convertors/calculators/CircleRadius.java
+++ src/com/EduardAliiev/utils/convertors/calculators/CircleRadius.java
@@ -14,15 +14,15 @@ public class CircleRadius
         System.out.println("Enter the radius_2 (r2) of the circle r2 = ");
         double radius2 = scanner.nextDouble();
 
-        if (radius1 >= radius2) {
+        if (radius1 == radius2) {
+            System.out.println("Please try again and enter uneven values the radius:");
+        } else if (radius1 >= radius2) {
             double circleArea = PI * radius1 * radius1;
             System.out.println("Larger Area1 of a circle is equal to " + circleArea);
-            }
-        else
-            if (radius1 <= radius2) {
-                double circleArea = PI * radius2 * radius2;
-                System.out.println("Larger Area2 of a circle is equal to " + circleArea);
-                }
+        } else if (radius1 <= radius2) {
+           double circleArea = PI * radius2 * radius2;
+           System.out.println("Larger Area2 of a circle is equal to " + circleArea);
+        }
     }
 }
 
