bd6698f4af549badd1adfea637b57b8db7c1ed56
EduardAliiev <edeku17@gmail.com>
2016-07-18 13:19:18 +0300

    Fixed issue in the looped

--- src/com/EduardAliiev/utils/convertors/runners/CalculateRunner.java
+++ src/com/EduardAliiev/utils/convertors/runners/CalculateRunner.java
@@ -10,9 +10,9 @@ public class CalculateRunner {
 
         public static void main(String args[]) {
             CalculateMenu.run();
-            CircleArea.calculateArea();
-            CircleRadius.calculateBigger();
-            EvenOrOdd.calculateEvenOrOdd();
-            AngledTriangle.calculateAngledTriangle();
+            //CircleArea.calculateArea();
+            //CircleRadius.calculateBigger();
+            //EvenOrOdd.calculateEvenOrOdd();
+            //AngledTriangle.calculateAngledTriangle();
         }
     }
