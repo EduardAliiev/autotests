9f29eb32998a11e2fead5ab794ce205bc1842403
EduardAliiev <edeku17@gmail.com>
2016-07-18 13:43:17 +0300

    add padding between the output information

--- src/com/EduardAliiev/utils/convertors/calculators/AngledTriangle.java
+++ src/com/EduardAliiev/utils/convertors/calculators/AngledTriangle.java
@@ -10,7 +10,7 @@ public class AngledTriangle {
 
         Scanner scanner = new
                 Scanner(System.in);
-        System.out.println("Enter the side angled triangle A =");
+        System.out.println("\n" + "Enter the side angled triangle A =");
         A = scanner.nextInt();
         System.out.println("Enter the side angled triangle B =");
         B = scanner.nextInt();
@@ -18,7 +18,7 @@ public class AngledTriangle {
             C = (A * A) + (B * B);
 
         if (C > A && C > B || A >= B && B >= A) {
-            System.out.println("Side angled triangle equal C =" + C * C);
+            System.out.println("\n" + "Side angled triangle equal C =" + C * C);
         }
     }
 }
