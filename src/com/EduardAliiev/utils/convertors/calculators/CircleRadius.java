fd273206e287931a1ab952a568548345f98ff8c6
EduardAliiev <edeku17@gmail.com>
2016-07-18 16:01:27 +0300

    Added some fields to improve

--- src/com/EduardAliiev/utils/convertors/calculators/CircleRadius.java
+++ src/com/EduardAliiev/utils/convertors/calculators/CircleRadius.java
@@ -9,14 +9,14 @@ public class CircleRadius
 
         Scanner scanner = new
                 Scanner(System.in);
-        System.out.println("Enter the radius_1 (r1) of the circle r1 = ");
+        System.out.println("\n" + "Enter the radius_1 (r1) of the circle r1 = ");
         double radius1 = scanner.nextDouble();
         System.out.println("Enter the radius_2 (r2) of the circle r2 = ");
         double radius2 = scanner.nextDouble();
 
         if (radius1 == radius2) {
-            System.out.println("Please try again and enter uneven values the radius!");
-        } else if (radius1 >= radius2) {
+            System.out.println("\n" + "Please try again and enter uneven values the radius!");
+        }  else if (radius1 >= radius2) {
             double circleArea = PI * radius1 * radius1;
             System.out.println("Larger Area1 of a circle is equal to " + circleArea);
         } else if (radius1 <= radius2) {
@@ -25,6 +25,8 @@ public class CircleRadius
         }
     }
 }
+
+
 
 
 
