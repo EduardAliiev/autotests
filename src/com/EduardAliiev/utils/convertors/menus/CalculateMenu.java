0e948a1dc66d6270ffd02cc89ca6352854db8483
EduardAliiev <edeku17@gmail.com>
2016-07-18 16:06:17 +0300

    added cycle  'do {} while' and operator 'break'

--- src/com/EduardAliiev/utils/convertors/menus/CalculateMenu.java
+++ src/com/EduardAliiev/utils/convertors/menus/CalculateMenu.java
@@ -9,32 +9,36 @@ public class CalculateMenu {
 
                 Scanner scanner = new
                         Scanner(System.in);
-                System.out.println("Welcome! Select one of the four programs is necessary to make next choice and Press Enter:" +
+                System.out.println("Welcome! Select one of the four programs is necessary to " +
+                        "make next choice and Press Enter:" + "\n" +
                         "\n" + "1 - Circle Area" + "\n" + "2 - Circle Radius" + "\n" + "3 - Right Angled Triangle" +
                         "\n" + "4 - Numbers Is Even Or Odd");
                 double numberOfChoice = scanner.nextDouble();
 
         do {
                 if (numberOfChoice == 1) {
+                    System.out.println("Entered number is = " + numberOfChoice);
                     CircleArea.calculateArea();
-                    break;
+                    //break;
                 } else if (numberOfChoice == 2) {
+                    System.out.println("Entered number is = " + numberOfChoice);
                     CircleRadius.calculateBigger();
-                    break;
+                    //break;
                 } else if (numberOfChoice == 3) {
+                    System.out.println("Entered number is = " + numberOfChoice);
                     AngledTriangle.calculateAngledTriangle();
-                    break;
+                    //break;
                 } else if (numberOfChoice == 4) {
+                    System.out.println("Entered number is = " + numberOfChoice);
                     EvenOrOdd.calculateEvenOrOdd();
-                    break;
+                    //break;
                 } else
                     System.out.println("You made wrong choice! :( Please try again later!");
-                    return;
+                    break;
         } while (numberOfChoice != 0);
-                System.out.println("Oh-yeah!! program has worked your choice! Good luck!!");
-
+                System.out.println("\n" + "Oh-yeah!! Program has worked your choice! Good luck!!");
+        //return;
     }
-
 }
 
 
