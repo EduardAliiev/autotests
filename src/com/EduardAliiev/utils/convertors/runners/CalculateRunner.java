20c2c61765cb4e333795adbf381bc011856a9f33
EduardAliiev <edeku17@gmail.com>
2016-07-18 15:59:31 +0300

    Fixed

--- src/com/EduardAliiev/utils/convertors/runners/CalculateRunner.java
+++ src/com/EduardAliiev/utils/convertors/runners/CalculateRunner.java
@@ -1,18 +1,14 @@
 package com.EduardAliiev.utils.convertors.runners;
 
 import com.EduardAliiev.utils.convertors.menus.CalculateMenu;
-import com.EduardAliiev.utils.convertors.calculators.CircleArea;
-import com.EduardAliiev.utils.convertors.calculators.CircleRadius;
-import com.EduardAliiev.utils.convertors.calculators.EvenOrOdd;
-import com.EduardAliiev.utils.convertors.calculators.AngledTriangle;
+//import com.EduardAliiev.utils.convertors.calculators.CircleArea;
+//import com.EduardAliiev.utils.convertors.calculators.CircleRadius;
+//import com.EduardAliiev.utils.convertors.calculators.EvenOrOdd;
+//import com.EduardAliiev.utils.convertors.calculators.AngledTriangle;
 
 public class CalculateRunner {
 
         public static void main(String args[]) {
             CalculateMenu.run();
-            //CircleArea.calculateArea();
-            //CircleRadius.calculateBigger();
-            //EvenOrOdd.calculateEvenOrOdd();
-            //AngledTriangle.calculateAngledTriangle();
         }
     }
