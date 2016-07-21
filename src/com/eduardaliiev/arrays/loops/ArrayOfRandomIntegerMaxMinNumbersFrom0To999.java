80b897538ca083c5fded4007ea03a3a6a0fa6d15
EduardAliiev <edeku17@gmail.com>
2016-07-21 12:39:17 +0300

    Minimum value is displayed of '0', as assigned 'int min = arraysNumber[0];'

--- src/com/eduardaliiev/arrays/loops/ArrayOfRandomIntegerMaxMinNumbersFrom0To999.java
+++ src/com/eduardaliiev/arrays/loops/ArrayOfRandomIntegerMaxMinNumbersFrom0To999.java
@@ -1,7 +1,25 @@
 package com.eduardaliiev.arrays.loops;
 
-/**
- * Created by edikaliiev on 7/20/16.
- */
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class ArrayOfRandomIntegerMaxMinNumbersFrom0To999 {
+    public static void calculateLoopNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        int[] arraysNumber = new int[15];
+        int max = arraysNumber[0];
+        int min = arraysNumber[0];
+        for (int i = 0; i < arraysNumber.length; i++) {
+            Random numbers = new Random();
+            arraysNumber[i] = numbers.nextInt(999);
+            System.out.print(arraysNumber[i] + " ");  // output of arrays line
+            if (arraysNumber[i] < min)
+                min = arraysNumber[i];
+            if (arraysNumber[i] > max)
+                max = arraysNumber[i];
+        }
+        System.out.println("\n" + "Min number " + min + "\n" + "Max number " + max);
+    }
 }
+
