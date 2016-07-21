221ebc82537c800507e43800b41597929a159f9f
EduardAliiev <edeku17@gmail.com>
2016-07-21 10:28:17 +0300

    All even elements of the array [2;20] are displayed vertically and horizontally.

--- src/com/eduardaliiev/arrays/loops/OddNumbersFrom1To99AndBack.java
+++ src/com/eduardaliiev/arrays/loops/OddNumbersFrom1To99AndBack.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 
 public class OddNumbersFrom1To99AndBack {
     public static void calculateLoopNumbers() {
-        Scanner scanner = new Scanner (System.in);
-        for (int A = 1; A <= 99; A++) {
-            System.out.print (A * 1 + " ");
+        Scanner scanner = new Scanner(System.in);
+        int A = 0;
+        int B = 100;
+        while (A <= 99) {
+            System.out.print((A += 1) + " ");
+            ++A;
+        } System.out.println(" ");
+        while (B >= 2) {
+            System.out.print((B -= 1) + " ");
+            --B;
         }
     }
 }
