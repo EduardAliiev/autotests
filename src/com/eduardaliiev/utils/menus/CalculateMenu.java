96e285f2c121a79e7226e92d14f1a7d0ff5593cf
EduardAliiev <edeku17@gmail.com>
2016-07-21 11:42:07 +0300

    Fixed output elements

--- src/com/eduardaliiev/utils/menus/CalculateMenu.java
+++ src/com/eduardaliiev/utils/menus/CalculateMenu.java
@@ -6,48 +6,37 @@ import java.util.Scanner;
 public class CalculateMenu {
 
     public static void run() {
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome! Select one of the four programs is necessary to " +
                 "make next choice and Press Enter:" + "\n" +
                 "\n" + "1 - Circle Area" + "\n" + "2 - Circle Radius" + "\n" + "3 - Right Angled Triangle" +
-                "\n" + "4 - Numbers Is Even Or Odd" + "\n" + "0 - Exit");
+                "\n" + "4 - Numbers Is Even Or Odd" + "\n" + "\n" + "5 - Exit");
         int numberOfChoice = (int) scanner.nextDouble();
+        do{
+            if (numberOfChoice == 1) {
+                System.out.println("Starting program: " + "Circle Area");
+                CircleArea.calculateArea();
+            } else if (numberOfChoice == 2) {
+                System.out.println("Starting program: " + "Circle Radius");
+                CircleRadius.calculateBigger();
+            } else if (numberOfChoice == 3) {
+                System.out.println("Starting program: " + "Right Angled Triangle");
+                AngledTriangle.calculateAngledTriangle();
+            } else if (numberOfChoice == 4) {
+                System.out.println("Starting program: " + "Numbers Is Even Or Odd");
+                EvenOrOdd.calculateEvenOrOdd();
+            } else if (numberOfChoice == 5) {
+                System.out.println("Starting program: " + "Exit");
+            } else
 
-        //if (numberOfChoice != 0) {
+                // } else {
+                //   System.out.println("You made wrong choice! :( Please try again later!");
+                System.out.println("\n" + "Oh-yeah!! Program has worked your choice! Good luck!!");
 
-            do{
-                if (numberOfChoice == 1) {
-                    System.out.println("Entered number is = " + numberOfChoice);
-                    CircleArea.calculateArea();
-                    //break;
-                } else if (numberOfChoice == 2) {
-                    System.out.println("Entered number is = " + numberOfChoice);
-                    CircleRadius.calculateBigger();
-                    //break;
-                } else if (numberOfChoice == 3) {
-                    System.out.println("Entered number is = " + numberOfChoice);
-                    AngledTriangle.calculateAngledTriangle();
-                    //break;
-                } else if (numberOfChoice == 4) {
-                    System.out.println("Entered number is = " + numberOfChoice);
-                    EvenOrOdd.calculateEvenOrOdd();
-                    //break;
-                } else if (numberOfChoice == 0) {
-                    System.out.println("Entered number is = " + numberOfChoice);
-                }
-                //else
-                //  System.out.println("You made wrong choice! :( Please try again later!");
-                // break;
-            } while (numberOfChoice != 0);
-        //} else {
-         //   System.out.println("You made wrong choice! :( Please try again later!");
-            // break;
-            System.out.println("\n" + "Oh-yeah!! Program has worked your choice! Good luck!!");
-           // return;
+        } while (numberOfChoice == 0);
+            //return;
         }
     }
-// }
 
 
 
