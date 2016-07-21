a6e6d407550c14f8152ccfe3d3574c03e3d685e0
EduardAliiev <edeku17@gmail.com>
2016-07-21 12:31:43 +0300

    Added file 'LoopsArraysRunner'

--- src/com/eduardaliiev/utils/runners/LoopsArraysRunner.java
+++ src/com/eduardaliiev/utils/runners/LoopsArraysRunner.java
@@ -1,6 +1,9 @@
 package com.eduardaliiev.utils.runners;
 
+import com.eduardaliiev.arrays.menus.LoopsArraysMenu;
+
 public class LoopsArraysRunner {
     public static void main(String args[]) {
+        LoopsArraysMenu.runloop();
     }
 }
