d851cc2b5ee830cffbc7bc445893231b47812948
EduardAliiev <edeku17@gmail.com>
2016-07-23 23:40:01 +0300

    Bubble Sorting method. The second method (keyboard input arrays: 'from smaller to bigger sorting').

---
+++ src/com/eduardaliiev/arrays/loops/BubbleSortEnteredKeyboardArray.java
@@ -0,0 +1,40 @@
+package com.eduardaliiev.arrays.loops;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSortEnteredKeyboardArray {
+
+    public static void calculateLoopBubbleSort() {
+        Scanner scanner = new Scanner(System.in);
+        int j;
+        boolean swapFlag = true;
+        int tempInt;
+        System.out.print("Please, enter size of the array: ");
+
+        int size = scanner.nextInt();
+        int[] arraysNumber = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element of the array: ");
+            arraysNumber[i] = scanner.nextInt();
+        }
+        System.out.print("\n" + "Original array:" + Arrays.toString(arraysNumber) + "\n ");
+        System.out.print("Sorted array: ");
+
+        for (int i = 0; i < arraysNumber.length; i++) {
+            while (swapFlag) {
+                swapFlag = false;
+                for (j = 0; j < arraysNumber.length - 1; j++) {
+                    if (arraysNumber[j] > arraysNumber[j + 1]) {
+                        tempInt = arraysNumber[j + 1];
+                        arraysNumber[j + 1] = arraysNumber[j];
+                        arraysNumber[j] = tempInt;
+                        swapFlag = true;
+                    }
+                }
+            }
+            System.out.print(arraysNumber[i] + " ");
+        }
+    }
+}
+
