4ae2552d17414f378310e329cfad0a4540220aa2
EduardAliiev <edeku17@gmail.com>
2016-07-23 15:04:55 +0300

    Added improvement: verification on input letters.

--- src/com/eduardaliiev/arrays/menus/LoopsArraysMenu.java
+++ src/com/eduardaliiev/arrays/menus/LoopsArraysMenu.java
@@ -25,43 +25,50 @@ public class LoopsArraysMenu {
                     "\n" + "5 - Two-dimensional array of random integer numbers from 1 to 99" +
                     "\n" + "6 - Bubble sotr from smaller to bigger and from bigger to smaller" + "\n" +
                     "\n" + "0 - Exit");
-            int numberOfChoice = (int) scanner.nextDouble();
-            switch (numberOfChoice) {
-                case 1:
-                    System.out.println("Starting program: " + "Even numbers from 2 to 20");
-                    EvenNumbersFrom2To20.calculateLoopNumbers();
-                    break;
-                case 2:
-                    System.out.println("Starting program: " + "Odd numbers from 1 to 99 and back");
-                    OddNumbersFrom1To99AndBack.calculateLoopNumbers();
-                    break;
-                case 3:
-                    System.out.println("Starting program: " + "Array of random integer numbers from 0 to 9");
-                    ArrayOfRandomIntegerNumbersFrom0To9.calculateLoopNumbers();
-                    break;
-                case 4:
-                    System.out.println("Starting program: " + "Array of random integer Max/Min numbers from 0 to 999");
-                    ArrayOfRandomIntegerMaxMinNumbersFrom0To999.calculateLoopNumbers();
-                    break;
-                case 5:
-                    System.out.println("Starting program: " + "Two-dimensional array of random integer numbers from 1 to 99");
-                    TwoDimensionalArrayOfRandomIntegerNumbersFrom1To99.calculateLoopNumbers();
-                    break;
-                case 6:
-                    System.out.println("Starting program: " + "Bubble sotr from smaller to bigger and from bigger to smaller");
-                    BubbleSort.calculateLoopBubbleSort();
-                    break;
-                case 0:
-                    System.out.println("Starting program: " + "Exit");
-                    break labelOfExit;
-                default:
-                    System.out.println("Sorry!! But this number isn't to select a program. Please try again to make your choice!!");
-                    break;     //break labelOfExit;           // для выхода из цикла
+            //int numberOfChoice = (int) scanner.nextDouble();
+            int numberOfChoice;
+            if (scanner.hasNextInt()) {                    // проверка на вводимое целое число
+                numberOfChoice = scanner.nextInt();
+
+                switch (numberOfChoice) {
+                    case 1:
+                        System.out.println("Starting program: " + "Even numbers from 2 to 20");
+                        EvenNumbersFrom2To20.calculateLoopNumbers();
+                        break;
+                    case 2:
+                        System.out.println("Starting program: " + "Odd numbers from 1 to 99 and back");
+                        OddNumbersFrom1To99AndBack.calculateLoopNumbers();
+                        break;
+                    case 3:
+                        System.out.println("Starting program: " + "Array of random integer numbers from 0 to 9");
+                        ArrayOfRandomIntegerNumbersFrom0To9.calculateLoopNumbers();
+                        break;
+                    case 4:
+                        System.out.println("Starting program: " + "Array of random integer Max/Min numbers from 0 to 999");
+                        ArrayOfRandomIntegerMaxMinNumbersFrom0To999.calculateLoopNumbers();
+                        break;
+                    case 5:
+                        System.out.println("Starting program: " + "Two-dimensional array of random integer numbers from 1 to 99");
+                        TwoDimensionalArrayOfRandomIntegerNumbersFrom1To99.calculateLoopNumbers();
+                        break;
+                    case 6:
+                        System.out.println("Starting program: " + "Bubble sotr from smaller to bigger and from bigger to smaller");
+                        BubbleSort.calculateLoopBubbleSort();
+                        break;
+                    case 0:
+                        System.out.println("Starting program: " + "Exit");
+                        break labelOfExit;
+                    default:
+                        System.out.println("Sorry!! But this number isn't to select a program. Please try again to make your choice!!");
+                        break;     //break labelOfExit;           // для выхода из цикла
+                }
+            } else {
+                System.out.println("Sorry!! But you have entered isn't an integer number! Please try again.");
+                break;                              // без 'break' программа зацикливается
             }
             System.out.println("\n" + "Oh-yeah!! Program has worked your choice! Good luck!!");
-        Thread.sleep(2500);         // задержка при выводе результата на 2,5 сек (1-3)
+            Thread.sleep(2500);         // задержка при выводе результата на 2,5 сек (1-3)
         }
-
     }
 }
 
--- src/com/eduardaliiev/utils/menus/CalculateMenu.java
+++ src/com/eduardaliiev/utils/menus/CalculateMenu.java
@@ -1,6 +1,7 @@
 package com.eduardaliiev.utils.menus;
 
 import com.eduardaliiev.utils.calculators.*;
+
 import java.util.Scanner;
 
 public class CalculateMenu {
@@ -9,37 +10,45 @@ public class CalculateMenu {
         Scanner scanner = new Scanner(System.in);
         labelOfExit:
         while (true) {
-        System.out.println("\n" + "Welcome! Select one of the four programs is necessary to " +
-                "make next choice and Press Enter:" + "\n" +
-                "\n" + "1 - Circle Area" +
-                "\n" + "2 - Circle Radius" +
-                "\n" + "3 - Right Angled Triangle" +
-                "\n" + "4 - Numbers Is Even Or Odd" + "\n" +
-                "\n" + "0 - Exit");
-        int numberOfChoice = (int) scanner.nextDouble();
-            switch (numberOfChoice) {
-                case 1:
-                    System.out.println("Starting program: " + "Circle Area");
-                    CircleArea.calculateArea();
-                    break;
-                case 2:
-                    System.out.println("Starting program: " + "Circle Radius");
-                    CircleRadius.calculateBigger();
-                    break;
-                case 3:
-                    System.out.println("Starting program: " + "Right Angled Triangle");
-                    AngledTriangle.calculateAngledTriangle();
-                    break;
-                case 4:
-                    System.out.println("Starting program: " + "Numbers Is Even Or Odd");
-                    EvenOrOdd.calculateEvenOrOdd();
-                    break;
-                case 0:
-                    System.out.println("Starting program: " + "Exit" + "\n" + "Good luck!!");
-                    break labelOfExit;
-                default:
-                    System.out.println("Sorry!! But this number isn't to select a program. Please try again to make your choice!!");
-                    break;     //break labelOfExit;           // для выхода из цикла
+            System.out.println("\n" + "Welcome! Select one of the four programs is necessary to " +
+                    "make next choice and Press Enter:" + "\n" +
+                    "\n" + "1 - Circle Area" +
+                    "\n" + "2 - Circle Radius" +
+                    "\n" + "3 - Right Angled Triangle" +
+                    "\n" + "4 - Numbers Is Even Or Odd" + "\n" +
+                    "\n" + "0 - Exit");
+            //int numberOfChoice = (int) scanner.nextDouble();
+            int numberOfChoice;
+            if (scanner.hasNextInt()) {                    // проверка на вводимое целое число
+                numberOfChoice = scanner.nextInt();
+
+                switch (numberOfChoice) {
+                    case 1:
+                        System.out.println("Starting program: " + "Circle Area");
+                        CircleArea.calculateArea();
+                        break;
+                    case 2:
+                        System.out.println("Starting program: " + "Circle Radius");
+                        CircleRadius.calculateBigger();
+                        break;
+                    case 3:
+                        System.out.println("Starting program: " + "Right Angled Triangle");
+                        AngledTriangle.calculateAngledTriangle();
+                        break;
+                    case 4:
+                        System.out.println("Starting program: " + "Numbers Is Even Or Odd");
+                        EvenOrOdd.calculateEvenOrOdd();
+                        break;
+                    case 0:
+                        System.out.println("Starting program: " + "Exit" + "\n" + "Good luck!!");
+                        break labelOfExit;
+                    default:
+                        System.out.println("Sorry!! But this number isn't to select a program. Please try again to make your choice!!");
+                        break;     //break labelOfExit;           // для выхода из цикла
+                }
+            } else {
+                System.out.println("Sorry!! But you have entered isn't an integer number! Please try again.");
+                break;                              // без 'break' программа зацикливается
             }
             System.out.println("\n" + "Oh-yeah!! Program has worked your choice! Good luck!!");
             Thread.sleep(2500);         // задержка при выводе результата на 2,5 сек (1-3)
