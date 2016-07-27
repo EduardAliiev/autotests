dbfc240fd238a78e1ce3e87ff093a36cfb50da47
EduardAliiev <edeku17@gmail.com>
2016-07-27 19:52:10 +0300

    Added: two-dimensional array of 8x5 values of 1 to 999 which are aligned on the right side

--- src/com/eduardaliiev/arrays/loops/TwoArrayOfRandomNumberAlignedOnRightFram1To999.java
+++ src/com/eduardaliiev/arrays/loops/TwoArrayOfRandomNumberAlignedOnRightFram1To999.java
@@ -1,16 +1,13 @@
 package com.eduardaliiev.arrays.loops;
 
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class TwoArrayOfRandomNumberAlignedOnRightFram1To999 {
-
     public static void calculateLoopNumbers() {
+
         Scanner scanner = new Scanner(System.in);
-
         int arraysNumbers[][] = new int[8][];
-
         for (int i = 0; i < arraysNumbers.length; i++) {
             arraysNumbers[i] = new int[5];
         }
@@ -18,8 +15,8 @@ public class TwoArrayOfRandomNumberAlignedOnRightFram1To999 {
             // System.out.println();
             for (int j = 0; j < arraysNumbers[i].length; j++) {
                 Random numbers = new Random();
-                arraysNumbers[i][j] = numbers.nextInt(10) + 1;
-                System.out.printf("%2d ", arraysNumbers[i][j]);
+                arraysNumbers[i][j] = numbers.nextInt(999) + 1;
+                System.out.printf("%2d  ", arraysNumbers[i][j]);
             }
             System.out.println();
         }
