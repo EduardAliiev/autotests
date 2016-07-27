a9d16ec434f050bd10c26a003fdf126bf00b52cf
EduardAliiev <edeku17@gmail.com>
2016-07-27 19:48:32 +0300

    Fixed output of 8x5 two-dimensional array of 10 to 99 (task #e)

--- src/com/eduardaliiev/arrays/loops/TwoArrayOfRandomNumberAlignedOnRightFram1To999.java
+++ src/com/eduardaliiev/arrays/loops/TwoArrayOfRandomNumberAlignedOnRightFram1To999.java
@@ -1,27 +1,27 @@
 package com.eduardaliiev.arrays.loops;
 
+
 import java.util.Random;
 import java.util.Scanner;
 
-public class TwoDimensionalArrayOfRandomIntegerNumbersFrom1To99 {
-    public static void calculateLoopNumbers() {
+public class TwoArrayOfRandomNumberAlignedOnRightFram1To999 {
 
+    public static void calculateLoopNumbers() {
         Scanner scanner = new Scanner(System.in);
+
         int arraysNumbers[][] = new int[8][];
 
         for (int i = 0; i < arraysNumbers.length; i++) {
             arraysNumbers[i] = new int[5];
         }
         for (int i = 0; i < arraysNumbers.length; i++) {
-            System.out.println();
+            // System.out.println();
             for (int j = 0; j < arraysNumbers[i].length; j++) {
                 Random numbers = new Random();
-                arraysNumbers[i][j] = numbers.nextInt(99) + 11;
-                System.out.print(arraysNumbers[i][j] + "    ");
+                arraysNumbers[i][j] = numbers.nextInt(10) + 1;
+                System.out.printf("%2d ", arraysNumbers[i][j]);
             }
-            //System.out.println();
+            System.out.println();
         }
     }
 }
-
-
