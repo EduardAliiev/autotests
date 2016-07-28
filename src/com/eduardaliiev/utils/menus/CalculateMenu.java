4e8165b1891383af69eff79ea8c10fe08e0ddec1
EduardAliiev <edeku17@gmail.com>
2016-07-28 15:55:58 +0300

    Menus fixed in accordance with 'Escape Characters'

--- src/com/eduardaliiev/utils/menus/CalculateMenu.java
+++ src/com/eduardaliiev/utils/menus/CalculateMenu.java
@@ -10,19 +10,22 @@ public class CalculateMenu {
         Scanner scanner = new Scanner(System.in);
         labelOfExit:
         while (true) {
-            System.out.println("\n" + "Welcome! Select one of the four programs is necessary to " +
-                    "make next choice and Press Enter:" + "\n" +
-                    "\n" + "1 - Circle Area" +
-                    "\n" + "2 - Circle Radius" +
-                    "\n" + "3 - Right Angled Triangle" +
-                    "\n" + "4 - Numbers Is Even Or Odd" + "\n" +
-                    "\n" + "0 - Exit");
+            System.out.println("\n Welcome! Select one of the four programs is necessary to " +
+                    "make next choice and Press Enter:\n" +
+                    "\n 1 - Circle Area" +
+                    "\n 2 - Circle Radius" +
+                    "\n 3 - Right Angled Triangle" +
+                    "\n 4 - Numbers Is Even Or Odd\n" +
+                    "\n 0 - Exit");
             //int numberOfChoice = (int) scanner.nextDouble();
             int numberOfChoice;
             if (scanner.hasNextInt()) {                    // проверка на вводимое целое число
                 numberOfChoice = scanner.nextInt();
 
                 switch (numberOfChoice) {
+                    case 0:
+                        System.out.println("Starting program: " + "Exit" + "\n" + "Good luck!!");
+                        break labelOfExit;
                     case 1:
                         System.out.println("Starting program: " + "Circle Area");
                         CircleArea.calculateArea();
@@ -39,9 +42,6 @@ public class CalculateMenu {
                         System.out.println("Starting program: " + "Numbers Is Even Or Odd");
                         EvenOrOdd.calculateEvenOrOdd();
                         break;
-                    case 0:
-                        System.out.println("Starting program: " + "Exit" + "\n" + "Good luck!!");
-                        break labelOfExit;
                     default:
                         System.out.println("Sorry!! But this number isn't to select a program. Please try again to make your choice!!");
                         break;     //break labelOfExit;           // для выхода из цикла
@@ -50,7 +50,7 @@ public class CalculateMenu {
                 System.out.println("Sorry!! But you have entered isn't an integer number! Please try again.");
                 break;                              // без 'break' программа зацикливается
             }
-            System.out.println("\n" + "Oh-yeah!! Program has processed your choice! Good luck!!");
+            System.out.println("\n Oh-yeah!! Program has processed your choice! Good luck!!");
             Thread.sleep(2500);         // задержка при выводе результата на 2,5 сек (1-3)
         }
     }
