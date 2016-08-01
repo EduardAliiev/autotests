a9e9ee0a56a643bbfa33b076a645d1eb04db6670
EduardAliiev <edeku17@gmail.com>
2016-08-01 13:51:26 +0300

    Fixet output of the maximum number

--- src/com/eduardaliiev/utils/arrays/loops/TwoArrayOfRandomNumberWithMaxNumberFromMinus99To99.java
+++ src/com/eduardaliiev/utils/arrays/loops/TwoArrayOfRandomNumberWithMaxNumberFromMinus99To99.java
@@ -8,26 +8,25 @@ public class TwoArrayOfRandomNumberWithMaxNumberFromMinus99To99 {
     public static void calculateLoopNumbers() {
 
         Scanner scanner = new Scanner(System.in);
-        int arraysNumbers[][] = new int[8][];
-        for (int i = 0; i < arraysNumbers.length; i++) {
-            arraysNumbers[i] = new int[5];
-        }
+        int arraysNumbers[][] = new int[5][8];
         for (int i = 0; i < arraysNumbers.length; i++) {
             for (int j = 0; j < arraysNumbers[i].length; j++) {
                 Random numbers = new Random();
                 arraysNumbers[i][j] = numbers.nextInt(199) - 99;            // section of a randome two-dimensional an array of [-99;99]
-                System.out.printf("%4d  ", arraysNumbers[i][j]);
+                System.out.printf("%3d  ", arraysNumbers[i][j]);
             }
             System.out.print("\n");
         }
-        System.out.println("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=");
+        System.out.println("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=");
 
-        int maxNegative = arraysNumbers[0][0];
-        for (int r = 0; r < arraysNumbers.length; r++) {
+        int maxNumber = arraysNumbers[0][0];
+        for (int r = 0; r < arraysNumbers.length; r++)
             for (int t = 0; t < arraysNumbers[r].length; t++)
-                if (arraysNumbers[r][t] < maxNegative) maxNegative = arraysNumbers[r][t];           // '>' - MAX positive number; '<' - MAX negative number
-        }
-        System.out.print("The maximum negative number = " + maxNegative + "\n");
+                if ((Math.abs(arraysNumbers[r][t])) > Math.abs(maxNumber))
+                    maxNumber = arraysNumbers[r][t];           // '>' - MAX positive number; '<' - MAX negative number
+
+        System.out.print("The maximum number = " + maxNumber + "\n");
     }
 }
+
 
