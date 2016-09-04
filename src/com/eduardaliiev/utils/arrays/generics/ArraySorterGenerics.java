8321dc5e8ef8f77aa7e76236b70c805ac3437c09
EduardAliiev <edeku17@gmail.com>
2016-09-04 19:47:04 +0300

    Added HomeWork 15(b)

---
+++ src/com/eduardaliiev/utils/arrays/generics/ArraySorterGenerics.java
@@ -0,0 +1,7 @@
+package com.eduardaliiev.utils.arrays.generics;
+
+/**
+ * Created by edikaliiev on 9/3/16.
+ */
+public class ArraySorterGenerics {
+}
---
+++ src/com/eduardaliiev/utils/runners/HomeTask15Runner.java
@@ -0,0 +1,9 @@
+package com.eduardaliiev.utils.runners;
+
+import com.eduardaliiev.utils.menus.GenericsMenuHomeTask15;
+
+public class HomeTask15Runner {
+    public static void main(String args[]) {
+        GenericsMenuHomeTask15.runnerGenerics();
+    }
+}
