27f9ffd3cd71119cee0020a18fbe65f403bf1f15
EduardAliiev <edeku17@gmail.com>
2016-09-16 13:17:08 +0300

    Added: two tasks at HomeTask16

--- src/main/java/com/eduardaliiev/app/arrays/generics/ArraySorterGenerics.java
+++ src/main/java/com/eduardaliiev/app/arrays/generics/ArraySorterGenerics.java
@@ -2,34 +2,36 @@ package com.eduardaliiev.app.arrays.generics;
 
 import java.util.Arrays;
 
-public class ArraySorterGenerics <K extends Number> {
+public class ArraySorterGenerics<K extends Number> {
+    K[] k;
 
-    private K[] k;
+  public static void genericsCreate() {
 
-    public static void main(String args[]) {
+        Integer[] arraysNumber = new Integer[15];
 
-        Integer[] arraysNumber = new Integer[5];
+        for (int i = 0; i < arraysNumber.length; i++) {
+            arraysNumber[i] = (arraysNumber.length - i);
+        }
 
-        for (int i = 0; i < arraysNumber.length; i++){
-            arraysNumber[i] = arraysNumber.length - 1;
-    }
         System.out.println(Arrays.toString((arraysNumber)));
         ArraySorterGenerics<Integer> arraySorterGenerics = new ArraySorterGenerics<Integer>(arraysNumber);
         arraySorterGenerics.sort();
         System.out.println(Arrays.toString(arraySorterGenerics.getK()));
     }
-    ArraySorterGenerics (K[] k) {
+
+    private ArraySorterGenerics(K[] k) {
         this.k = k;
     }
 
-    public void sort(){
+    public void sort() {
         this.k = staticSort(k);
     }
+
     public K[] getK() {
         return k;
     }
 
-    public  static <K extends Number> K[] staticSort (K[] k) {
+    public static <K extends Number> K[] staticSort(K[] k) {
         for (int i = 0; i < k.length - 1; i++) {
             K temp;
             for (int j = 1; j < k.length - i; j++) {
