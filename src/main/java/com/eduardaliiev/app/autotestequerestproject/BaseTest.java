8997dd12f1bf9fdc2ec5988406c201e56dcfcdeb
EduardAliiev <edeku17@gmail.com>
2016-10-02 22:27:49 +0300

    added: chrome path for the OS win and mac

--- src/main/java/com/eduardaliiev/app/autotestequerestproject/BaseTest.java
+++ src/main/java/com/eduardaliiev/app/autotestequerestproject/BaseTest.java
@@ -4,6 +4,7 @@ package com.eduardaliiev.app.autotestequerestproject;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -12,19 +13,49 @@ import java.util.concurrent.TimeUnit;
  * Created by Eduard Aliiev on 9/25/16;
  */
 public class BaseTest {
-  protected WebDriver driver;
-   // String baseUrl = "https://dev.equerest.com/";
+
+    protected WebDriver driver;
+    private String BROWSER = System.getProperty("browser");
+    private static String OS = System.getProperty("os.name").toLowerCase();
+
+    private static final String CHROME_PATH_MAC = "src/main/java/com/eduardaliiev/app/autotestequerestproject/resources/drivers/chromedriver";
+    private static final String CHROME_PATH_WIN = "src/main/java/com/eduardaliiev/app/autotestequerestproject/resources/drivers/chromedriver.exe";
 
     @Before
     public void setUp() {
-        this.driver = new FirefoxDriver();
+
+        //mvn clean test -Dbrowser="Chrome"
+
+        if ( BROWSER == null || BROWSER.equalsIgnoreCase("Firefox") || BROWSER.equalsIgnoreCase("")) {
+            this.driver = new FirefoxDriver();
+        } else if (BROWSER.equalsIgnoreCase("Chrome")) {
+            if (isWindows()) {
+                System.setProperty("webdriver.chrome.driver", CHROME_PATH_WIN);
+            } else if (isMac()){
+                System.setProperty("webdriver.chrome.driver", CHROME_PATH_MAC);
+            }
+            this.driver = new ChromeDriver();
+        }
+
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-       // driver.get(baseUrl + "register#/");
-
     }
 
     @After
-    public void tearDown() { driver.close(); }
+    public void tearDown() {
+        driver.close();
+    }
+
+    private static boolean isWindows() {
+        return (OS.contains("win"));
+    }
+
+    private static boolean isMac() {
+        return (OS.contains("mac"));
+    }
+
+    public static boolean isUnix() {
+        return (OS.contains("nix") || OS.contains("nux") || OS.contains("aix"));
+    }
 
 }
