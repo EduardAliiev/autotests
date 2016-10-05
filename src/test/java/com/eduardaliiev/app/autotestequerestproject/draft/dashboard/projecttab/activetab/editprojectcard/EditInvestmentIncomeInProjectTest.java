960f152248c07976815b0ed0a77bc95c8897d1bd
EduardAliiev <edeku17@gmail.com>
2016-10-05 20:13:12 +0300

    added: @Title and @Description

--- src/test/java/com/eduardaliiev/app/autotestequerestproject/draft/dashboard/projecttab/activetab/editprojectcard/EditInvestmentIncomeInProjectTest.java
+++ src/test/java/com/eduardaliiev/app/autotestequerestproject/draft/dashboard/projecttab/activetab/editprojectcard/EditInvestmentIncomeInProjectTest.java
@@ -7,12 +7,23 @@ import junitparams.FileParameters;
 import junitparams.JUnitParamsRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Title;
 
 /**
  * Created by Eduard Aliiev on 9/27/16;
  */
+@Title ("TC-05 'Редактирование инвестиционного дохода'")
+@Description("- Пользователь [login] должен предварительно войти в систему, используя [password]\n" +
+        "- Проект [projectName] должен быть в статусе \"Активные\"")
+
+
 @RunWith(JUnitParamsRunner.class)
 public class EditInvestmentIncomeInProjectTest extends BaseTest{
+
+    @Title("dgfdg")
+    @Description("dfjkhgskdf vksdnfvkejrgnvjdfnv")
+
     private EditInvestmentIncomeInProject editValueInvestment(String login, String password, String projectName) {
         MainPage mainPage = new MainPage("https://dev.equerest.com/", driver);
         ProjectActivePage projectActivePage = mainPage.openLoginPage()
@@ -22,7 +33,6 @@ public class EditInvestmentIncomeInProjectTest extends BaseTest{
         return projectActivePage.navigateToActiveProjectPageViaMenuItem()
                 .searchActiveProjectByName(projectName);
     }
-    //"Активные" section is delete video link.
     @Test
     @FileParameters("src/main/java/com/eduardaliiev/app/autotestequerestproject/resources/login_project_info_Active.csv")
     public void editInvestmentIncome(String login, String password, String projectName) {
