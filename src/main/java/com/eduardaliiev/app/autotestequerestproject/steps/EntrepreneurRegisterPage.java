9affef6d74017fb36afc5b9f80a9f2f774684c12
EduardAliiev <edeku17@gmail.com>
2016-10-07 18:18:24 +0300

    added: 'EntrepreneurRegisterPage.java'

---
+++ src/main/java/com/eduardaliiev/app/autotestequerestproject/steps/EntrepreneurRegisterPage.java
@@ -0,0 +1,25 @@
+package com.eduardaliiev.app.autotestequerestproject.steps;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by Eduard Aliiev on 10/7/16;
+ */
+public class EntrepreneurRegisterPage {
+    public WebDriver driver;
+
+    @FindBy(id = "user_first_name")
+    private WebElement firstNameTextBox;
+
+    public EntrepreneurRegisterPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void she_provides_the_first_name_as(String firstName) throws Throwable {
+        firstNameTextBox.sendKeys(firstName);
+    }
+}
