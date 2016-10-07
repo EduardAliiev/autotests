e17ffe6fd58b9c43f46f295c7ae856d3995e456b
EduardAliiev <edeku17@gmail.com>
2016-10-07 18:13:47 +0300

    added: 'steps' folder and 'DriverFactory' file

---
+++ src/main/java/com/eduardaliiev/app/autotestequerestproject/steps/DriverFactory.java
@@ -0,0 +1,12 @@
+package com.eduardaliiev.app.autotestequerestproject.steps;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Eduard Aliiev on 10/7/16;
+ */
+public class DriverFactory {
+    protected static WebDriver driver = new FirefoxDriver();
+
+}
