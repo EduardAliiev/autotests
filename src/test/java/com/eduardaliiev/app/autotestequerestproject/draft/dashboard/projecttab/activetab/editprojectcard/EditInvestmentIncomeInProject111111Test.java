e4ab67accd934abbcb8b75a828a2da1671ca0813
EduardAliiev <edeku17@gmail.com>
2016-11-29 17:58:22 +0200

    several fixes

---
+++ src/test/java/com/eduardaliiev/app/autotestequerestproject/draft/dashboard/projecttab/activetab/editprojectcard/EditInvestmentIncomeInProject111111Test.java
@@ -0,0 +1,34 @@
+package com.eduardaliiev.app.autotestequerestproject.draft.dashboard.projecttab.activetab.editprojectcard;
+
+import org.junit.Test;
+
+/**
+ * Created by Eduard Aliiev on 11/21/16;
+ */
+public class EditInvestmentIncomeInProject111111Test {
+    @Test
+    public void editAndSaveStandardProjectField() throws Exception {
+
+    }
+
+    @Test
+    public void investmentIncomeValue() throws Exception {
+
+    }
+
+    @Test
+    public void searchActiveProjectByName() throws Exception {
+
+    }
+
+    @Test
+    public void navigateModerationToProjectPageViaMenuItem() throws Exception {
+
+    }
+
+    @Test
+    public void navigateToActiveProjectPageViaMenuItem() throws Exception {
+
+    }
+
+}
